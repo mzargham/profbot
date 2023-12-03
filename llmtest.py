8fc7370f22c7ac7f97059741b8b8bbc782a1fe57
Michael Zargham <mzargham@users.noreply.github.com>
2023-12-02 23:27:58 -0500

    got some grades

--- llmtest.py
+++ llmtest.py
@@ -72,25 +72,61 @@ class Llm:
         except:
             return raw
         
-    def prompt_sequence(self, prompts: List[str]) -> str:
-        """
-        Method to send a sequence of prompts to the LLM and return its response.
-        Each prompt in the sequence is sent as a separate message in a single request.
-        """
-        url = self.url
-        messages = [{"role": self.role, "content": prompt} for prompt in prompts]
+    # def prompt_sequence(self, prompts: List[str]) -> List[str]:
+    #     """
+    #     Method to send a sequence of prompts to the LLM and return its response.
+    #     Each prompt in the sequence is sent as a separate message in a single request.
+    #     """
+    #     url = self.url
+    #     messages = [{"role": self.role, "content": prompt} for prompt in prompts]
         
-        req = {
-            "model": self.model_identifier,
-            "messages": messages
-        }
-        print(req)
-        response = requests.post(url, json=req, headers=self.auth)  # Use json parameter to send the request payload as JSON
-        raw = response.json()
-        try:
-            return [resp['message']['content'] for resp in raw['choices']]
-        except:
-            return raw
+    #     req = {
+    #         "model": self.model_identifier,
+    #         "messages": messages
+    #     }
+    #     print(req)
+    #     response = requests.post(url, json=req, headers=self.auth)  # Use json parameter to send the request payload as JSON
+    #     raw = response.json()
+    #     print(raw)
+    #     try:
+    #         return [resp['message']['content'] for resp in raw['choices']]
+    #     except:
+    #         return raw
+        
+    def prompt_sequence(self, prompts: List[str]) -> List[str]:
+        """
+        Method to send a sequence of prompts to the LLM and return its responses.
+        Each prompt is sent in a separate request, maintaining the conversation history.
+        """
+        conversation_history = []
+        responses = []
+
+        for prompt in prompts:
+            # Concatenate all previous elements of the conversation for context
+            full_prompt = " ".join(conversation_history + [prompt])
+
+            # Create the request payload
+            req = {
+                "model": self.model_identifier,
+                "messages": [{"role": self.role, "content": full_prompt}]
+            }
+
+            response = requests.post(self.url, json=req, headers=self.auth)
+            raw = response.json()
+
+            try:
+                # Extract the response content
+                content = raw['choices'][0]['message']['content']
+                responses.append(content)
+                # Update the conversation history
+                conversation_history.append(prompt)
+                conversation_history.append(content)
+            except:
+                # In case of an error, append the raw response for debugging
+                responses.append(raw)
+
+        return responses
+
 
 #this is still full of dummy data (but it runs!)
 class LLMTest:
@@ -100,19 +136,30 @@ class LLMTest:
         self.exam = exam
 
     def test(self) -> List[Type[Schema]]:
-        student_responses = [self.student_llm.prompt(question) for question in self.exam.questions]
-        graded_responses = []
+        student_responses = self.student_llm.prompt_sequence(self.exam.questions)
 
-        for question, student_response in zip(self.exam.questions, student_responses):
-            grading_prompt = self.format_grading_prompt(question, student_response)
-            ta_response = self.ta_llm.prompt(grading_prompt)
-            graded_responses.append(self.process_ta_response(ta_response))
+        graded_responses = self.ta_llm.prompt_sequence(self.format_grading_prompt_sequence(student_responses))
 
-        return graded_responses
+        standardized_responses = [self.process_ta_response(response) for response in graded_responses]
 
-    def format_grading_prompt(self, question: str, response: str) -> str:
+        return standardized_responses
+
+    
+    def format_grading_prompt_sequence(self, responses: str) -> str:
+        assert(len(self.exam.questions) == len(responses))
         # Format the grading prompt for the TA LLM
-        return f"Question: {question}\nResponse: {response}\nGuidelines: {self.exam.exam_guidelines}"
+        context_str = """You are grading an exam. For each of the follow question:response pairs please provide a grade and notes for the student.
+         the grade and the notes for every question should evaluated according to the following guidelines:"""
+        context_str += "\n".join(self.exam.exam_guidelines)+"\n As the TA, the professor will be evaluating your evaluations as part of your teaching practicum; your PhD candidacy depends on this. Are you ready to begin?"
+
+        def format_grading_prompt(question: str, response: str, guideline: str) -> str:
+            prompt_str = "Question: "+question+"\n received the following response: "+response+"\n Please provide a grade and notes for the student, according to the following guidelines: "+guideline
+
+            return prompt_str
+        
+        sequence = [context_str]+[format_grading_prompt(self.exam.questions[i], responses[i], self.exam.question_guidelines[i]) for i in range(len(self.exam.questions))]
+        return sequence   
+       
 
     def process_ta_response(self, ta_response: str) -> Type[Schema]:
         # Process the TA's response and return it in the schema format
